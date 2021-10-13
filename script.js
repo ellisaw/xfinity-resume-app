b2f220e6ba52329ec08d9e731310931a4c0f2953
elluhs <52143888+elluhs@users.noreply.github.com>
2021-10-13 19:05:53 -0400

    update frame height

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 window.onload = function() {
   var frame = document.getElementById("page1");
   var frameHeight = frame.contentWindow.document.body.scrollHeight + 'px';
-  console.log(frameHeight);
   setFrameHeight();
 
   function setFrameHeight() {
     console.log("hello");
-    document.getElementById("page1-holder").style.height = "160px";
+    document.getElementById("page1-holder").style.height = frameHeight;
   }
 }
